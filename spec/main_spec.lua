f85e0eab85c40f7be49bce8996130514d5b421ea
Maciej Kucharski <maciek.zir@gmail.com>
2015-06-01 16:49:00 +0200

    [master] metatable based solution

--- spec/main_spec.lua
+++ spec/main_spec.lua
@@ -1,10 +1,76 @@
 Tennis = require "tennis"
-describe("sanity", function()
-  it("is sane", function()
-    assert.is_true(true)
+
+describe("Tennis", function()
+  local tennis
+
+  before_each(function()
+    tennis = Tennis.create()
   end)
 
-  it("Tennis", function ()
-    t = Tennis();
+  after_each(function() tennis = nil end)
+
+  describe("#create", function()
+    it("returns a Tennis insteance object", function()
+      assert(getmetatable(tennis) == Tennis)
+    end)
+
+    it("sets player 1 score to 0", function ()
+      assert.equal(tennis.score[1], 0)
+    end)
+
+    it("sets player 2 score to 0", function ()
+      assert.equal(tennis.score[2], 0)
+    end)
   end)
+
+  describe("#set_score", function()
+    it("correctly sets the score", function()
+      tennis:set_score({ 5, 5 })
+
+      assert.same(tennis.score, { 5, 5 })
+    end)
+  end)
+
+  describe("#get_score", function()
+
+    local test_cases = {
+      ['Love, Love']       = { 0, 0 },
+      ['Fifteen, Love']    = { 1, 0 },
+      ['Thirty, Love']     = { 2, 0 },
+      ['Forty, Love']      = { 3, 0 },
+      ['Fifteen, Fifteen'] = { 1, 1 },
+      ['Adv. player 1']    = { 4, 3 },
+      ['Adv. player 2']    = { 5, 6 },
+      ['Deuce']            = { 4, 4 },
+      ['Winner: player 1'] = { 5, 3 },
+      ['Winner: player 2'] = { 0, 4 },
+
+    }
+
+    for result, input in pairs(test_cases) do
+      it("returns '"..result.."' when players scored "..input[1]..':'..input[2],
+      function()
+        tennis:set_score(input)
+        assert.are.same(tennis:get_score(), result)
+      end)
+    end
+
+
+
+  end)
+
+  describe("#increase_score", function()
+    it("errors when player index isn't specified", function()
+      assert.has_error(function () tennis:increase_score() end)
+    end)
+
+    it("increments the score of a player", function()
+      assert.equal(tennis.score[1], 0)
+
+      tennis:increase_score(1)
+      assert.equal(tennis.score[1], 1)
+    end)
+
+  end)
+
 end)
--- tennis.lua
+++ tennis.lua
@@ -1,8 +1,70 @@
-function Tenis()
+Tennis = {}
+Tennis.__index = Tennis
 
+function Tennis.create()
+  local self = setmetatable({}, Tennis)
+  self.score = {0, 0}
 
-  return {}
+  return self
 end
 
+function Tennis:translate_score(score)
+  local lookup = {
+    [0] = 'Love',
+    [1] = 'Fifteen',
+    [2] = 'Thirty',
+    [3] = 'Forty'
+  }
 
-return Tenis
+  return lookup[score] or ""
+end
+
+function Tennis:test_advantage()
+  if self.score[1] >= 4 and self.score[1] == (self.score[2] + 1) then
+    return "Adv. player 1"
+  end
+
+  if self.score[2] >= 4 and self.score[2] == self.score[1] + 1 then
+    return "Adv. player 2"
+  end
+end
+
+function Tennis:test_deuce()
+  return self.score[1] == self.score[2] and self.score[1] >= 4
+end
+
+function Tennis:test_winner()
+  if self.score[1] >= 4 and self.score[1] >= (self.score[2] + 2) then
+    return "Winner: player 1"
+  end
+
+  if self.score[2] >= 4 and self.score[2] >= (self.score[1] + 2) then
+    return "Winner: player 2"
+  end
+end
+
+function Tennis:get_score()
+  local score1, score2 =  self:translate_score(self.score[1]),
+                          self:translate_score(self.score[2])
+
+  local advantage, deuce, winner =  self:test_advantage(),
+                                    self:test_deuce(),
+                                    self:test_winner()
+
+  if advantage then return advantage end
+  if deuce then return "Deuce" end
+  if winner then return winner end
+
+  return score1..', '..score2
+end
+
+function Tennis:set_score(score)
+  self.score = score
+end
+
+function Tennis:increase_score(playerIdx)
+  if playerIdx == nil then error("Must pass player index") end
+  self.score[playerIdx] = self.score[playerIdx] + 1
+end
+
+return Tennis
